e43b890b98487a1278b2a0d12b4c3a9aec9aab0d
Nghia Cao <nghiacvt81@gmail.com>
2026-01-29 22:59:56 +0700

    cập nhật code.gs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // 1. CẤU HÌNH & BIẾN TOÀN CỤC
 // ==========================================
 // URL Web App từ Google Apps Script (Đã cấu hình doPost)
-const API_URL = "https://script.google.com/macros/s/AKfycbw7sAYlX51tTO7uHC0-gq1DWW5B3uyVQKgloUY6dPPZ2vcxtkdnrI-od7C5g9AiaT5wEg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxrpjTCpEqpdymF3w9YxiweMWSJSpRf3fRVC7Vms-JhFGR_6p8yikj_eheh7Afxr6lGpA/exec";
 
 var currentUser = null;
 var videoStream = null;
@@ -1557,6 +1557,7 @@ function updateClock() {
   setText("clock-display", timeStr);
   setText("date-display", dateStr);
 }
+
 
 
 
